e70eda91a580c8b9b9043927c27ca05d4704df29
xinran0722l <xinrany2@andrew.cmu.edu>
2023-04-20 23:34:14 -0400

    Initial Commit

---
+++ MEAL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEAL
+//
+//  Created by Xinran Yu on 4/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MEALUITests/MEALUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MEALUITestsLaunchTests.swift
+//  MEALUITests
+//
+//  Created by Xinran Yu on 4/20/23.
+//
+
+import XCTest
+
+final class MEALUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
